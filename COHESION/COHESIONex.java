e83b4c8aef73f1479a177740807e6efc9e0ba15e
Satyam Shrivastava <satyam62621762@gmail.com>
2026-09-20 22:34:32 +0530

    Add files via upload

---
+++ COHESION/COHESIONex.java
@@ -0,0 +1,20 @@
+ //Highly cohesive class  
+class Calculator {  
+  
+    int add(int a, int b) {  
+        return a + b;  
+    }  
+  
+    int subtract(int a, int b) {  
+        return a - b;  
+    }  
+}  
+  
+public class COHESIONex {  
+    public static void main(String[] args) {  
+        Calculator calc = new Calculator();  
+  
+        System.out.println("Addition: " + calc.add(10, 5));  
+        System.out.println("Subtraction: " + calc.subtract(10, 5));  
+    }  
+}  
