e09e24109278ecfff839acf485dfdbfe155639b3
Satyam Shrivastava <satyam62621762@gmail.com>
2026-09-22 22:46:14 +0530

    Add files via upload

---
+++ ENCAPSULATION/Stringmanipulation.java
@@ -0,0 +1,10 @@
+package OVERLOADING;
+
+public class Stringmanipulation {  
+    public String concatenate(String str1, String str2) {  
+        return str1 + str2;  
+    }  
+    public String concatenate(String str1, String str2, String str3) {  
+        return str1 + str2 + str3;  
+    }  
+}  
---
+++ ENCAPSULATION/polyOLmathadd.java
@@ -0,0 +1,9 @@
+
+public class polyOLmathadd {  
+    public int add(int a, int b) {  
+        return a + b;  
+    }  
+    public double add(double a, double b, double c) {  
+        return a + b + c;  
+    }  
+}  
