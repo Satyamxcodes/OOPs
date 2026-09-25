ffdda242f6c01c3e7be1acc586b694fe227ded09
Satyam Shrivastava <satyam62621762@gmail.com>
2026-09-25 19:47:13 +0530

    Add files via upload

---
+++ ENCAPSULATION/hellosub.java
@@ -0,0 +1,7 @@
+
+    
+class hellosub {    
+  public static void main(String args[]){    
+   System.out.println("Hello subpackage");    
+  }    
+}    
---
+++ ENCAPSULATION/importallclass.java
@@ -0,0 +1,12 @@
+
+public class importallclass {  
+    static void foo() {  
+                         // we are writing the class name along with the package name  
+        java.util.ArrayList<Integer> al = new java.util.ArrayList<Integer>();  
+        al.add(1);
+    }  
+public static void main(String args[]) {  
+      foo();  
+      System.out.println("Inside the main method");  
+}  
+}  
